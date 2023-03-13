d58588975652d4bc5d810a61d3c6941452a34ce3
Yozakura Sen <yozakura-sen@outlook.com>
2023-03-13 11:06:40 +0800

    1.0.0

--- src/main/java/com/yozakura/ExternalCommand/ExCMD.java
+++ src/main/java/com/yozakura/ExternalCommand/ExCMD.java
@@ -4,6 +4,9 @@ import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+
+import static com.yozakura.ExternalCommand.ExMod.logger;
 
 public class ExCMD extends CommandBase {
     @Override
@@ -18,11 +21,16 @@ public class ExCMD extends CommandBase {
 
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-        ExUI.main(args,server);
+        try {
+            ExUI.main(args, server);
+            logger.info("ExUI was opened");
+        } catch (Exception exception) {
+            logger.error("ExUI was error");
+        }
     }
 
     @Override
     public int getRequiredPermissionLevel() {
-        return 0;
+        return 4;
     }
 }
--- src/main/java/com/yozakura/ExternalCommand/ExMod.java
+++ src/main/java/com/yozakura/ExternalCommand/ExMod.java
@@ -13,7 +13,7 @@ public class ExMod
     public static final String NAME = "External Command";
     public static final String VERSION = "1.0.0";
 
-    private static Logger logger;
+    public static Logger logger;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
